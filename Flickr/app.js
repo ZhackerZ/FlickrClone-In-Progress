0b7ce560a4c0d08f46d965ba5d12f3946473bcc5
Yuzhen <liu.2699@buckeyemail.osu.edu>
2016-07-04 22:49:55 -0700

    add basic user authentication and cookie parser

--- Flickr/app.js
+++ Flickr/app.js
@@ -31,7 +31,39 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+function auth (req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+        return;
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        err.status = 401;
+        next(err);
+    }
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err,req,res,next) {
+            res.writeHead(err.status || 500, {
+            'WWW-Authenticate': 'Basic',
+            'Content-Type': 'text/plain'
+        });
+        res.end(err.message);
+});
 
 app.use('/', routes);
 app.use('/users', users);
