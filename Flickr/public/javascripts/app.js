a93f008887c1b6b3b891e3b5aeb7a15e6eafbe60
Yuzhen <liu.2699@buckeyemail.osu.edu>
2016-07-20 20:22:51 -0700

    Remove React add Angular Scope variable

--- Flickr/public/javascripts/app.js
+++ Flickr/public/javascripts/app.js
@@ -1,70 +1,77 @@
 'use strict';
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 1;
-  this.filtText = '';
+angular.module('confusionApp', []).controller('MenuController', ['$scope', function() {
+  $scope.tab = 1;
+  $scope.filtText = '';
   
-  var dishes = [
-    {
-      name:'Uthapizza',
-      image: 'images/uthapizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: ''
-    },
+  // var dishes = [
+  //   {
+  //     name:'Uthapizza',
+  //     image: 'images/uthapizza.png',
+  //     category: 'mains',
+  //     label:'Hot',
+  //     price:'4.99',
+  //     description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+  //     comment: ''
+  //   },
     
-    {
-      name:'Zucchipakoda',
-      image: 'images/zucchipakoda.png',
-      category: 'appetizer',
-      label:'',
-      price:'1.99',
-      description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-      comment: ''
-    },
+  //   {
+  //     name:'Zucchipakoda',
+  //     image: 'images/zucchipakoda.png',
+  //     category: 'appetizer',
+  //     label:'',
+  //     price:'1.99',
+  //     description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+  //     comment: ''
+  //   },
 
-    {
-      name:'Vadonut',
-      image: 'images/vadonut.png',
-      category: 'appetizer',
-      label:'New',
-      price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-      comment: ''
-    },
+  //   {
+  //     name:'Vadonut',
+  //     image: 'images/vadonut.png',
+  //     category: 'appetizer',
+  //     label:'New',
+  //     price:'1.99',
+  //     description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+  //     comment: ''
+  //   },
     
-    {
-      name:'ElaiCheese Cake',
-      image: 'images/elaicheesecake.png',
-      category: 'dessert',
-      label:'',
-      price:'2.99',
-      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-      comment: ''
-    }
-  ]; 
+  //   {
+  //     name:'ElaiCheese Cake',
+  //     image: 'images/elaicheesecake.png',
+  //     category: 'dessert',
+  //     label:'',
+  //     price:'2.99',
+  //     description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+  //     comment: ''
+  //   }
+  // ]; 
+
+
+  $scope.showDetails = false;
+
+  $scope.toggleDetails = function() {
+    $scope.showDetails = !$scope.showDetails;
+  };
   
-  this.dishes = dishes;
+  $scope.dishes = dishes;
 
-  this.select = function(setTab) {
-    this.tab = setTab;
+  $scope.select = function(setTab) {
+    $scope.tab = setTab;
     
     if (setTab === 2) {
-      this.filtText = "appetizer";
+      $scope.filtText = "appetizer";
     } 
     else if (setTab === 3) {
-      this.filtText = "mains";
+      $scope.filtText = "mains";
     }
     else if (setTab === 4) {
-      this.filtText = "dessert";
+      $scope.filtText = "dessert";
     }
     else {
-      this.filtText = "";
+      $scope.filtText = "";
     }
   };
   
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
+  $scope.isSelected = function (checkTab) {
+    return ($scope.tab === checkTab);
   };
-});
+}]);
